7234cad6ecbee19528e3d5150472ac26972f8f4f
sparsh13b <sparshbirla1@gmail.com>
2026-02-12 20:09:50 +0530

    changed sending email address

--- backend/src/services/email.service.js
+++ backend/src/services/email.service.js
@@ -1,11 +1,11 @@
 const transporter = require('../config/mailer');
 
-exports.sendEmail = async ({ to, task, assignedBy}) => {
-    const mailOptions = {
-        from: `"${assignedBy.name} via Task Manager" <${process.env.EMAIL_USER}>`,
-        to,
-        subject: `New Task Assigned: ${task.title}`,
-        html: `
+exports.sendEmail = async ({ to, task, assignedBy }) => {
+  const mailOptions = {
+    from: `"${assignedBy.name} via TaskPal" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: `New Task Assigned: ${task.title}`,
+    html: `
       <h2>You have been assigned a new task</h2>
       <p><strong>Title:</strong> ${task.title}</p>
       <p><strong>Description:</strong> ${task.description}</p>
@@ -13,24 +13,24 @@ exports.sendEmail = async ({ to, task, assignedBy}) => {
       <p><strong>Due Date:</strong> ${new Date(task.dueDate).toDateString()}</p>
       <p><strong>Assigned By:</strong> ${assignedBy.name} (${assignedBy.email})</p>
       <hr />
-      <p>Please log in to the system to view and update the task.</p>
+      <p>Please log in to TaskPal to view and update your task.</p>
     `,
-    };
+  };
 
-    await transporter.sendMail(mailOptions);
+  await transporter.sendMail(mailOptions);
 };
 
 exports.sendReminderEmail = async ({ to, task }) => {
-    const mailOptions = {
-      from: `"Task Manager" <${process.env.EMAIL_USER}>`,
-      to,
-      subject: `Reminder: Task Due Soon - ${task.title}`,
-      html: `
+  const mailOptions = {
+    from: `"TaskPal" <${process.env.EMAIL_USER}>`,
+    to,
+    subject: `Reminder: Task Due Soon - ${task.title}`,
+    html: `
         <h2>Task Reminder</h2>
         <p>Your task <strong>${task.title}</strong> is due soon.</p>
         <p><strong>Due Date:</strong> ${new Date(task.dueDate).toDateString()}</p>
         <p>Please make sure to complete it on time.</p>      
         `,
-    };
-    await transporter.sendMail(mailOptions);
   };
+  await transporter.sendMail(mailOptions);
+};
